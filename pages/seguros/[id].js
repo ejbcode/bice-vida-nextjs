b7b84bf814b3d2527f956c71239681961fd381ef
Ernesto B <ejbcode@gmail.com>
2021-04-05 15:41:31 -0400

    adding id routes

---
+++ pages/seguros/[id].js
@@ -0,0 +1,36 @@
+const InsuranceItem = ({ insurance }) => {
+  const { id, name, image, description, price } = insurance
+  return (
+    <div>
+
+      Static
+      <p>{name}</p>
+      <img src={image} alt={name} />
+      <pre><code>{JSON.stringify(insurance, null, 4)}</code></pre>
+    </div>
+  )
+}
+
+export const getStaticProps = async ({ params }) => {
+  const res = await fetch(`https://bice-vida-nextjs.vercel.app/api/seguros/${params.id}`)
+  const insurance = await res.json()
+  return {
+    props: {
+      insurance,
+    },
+  }
+}
+
+export const getStaticPaths = async () => {
+  const res = await fetch(`https://bice-vida-nextjs.vercel.app/api/seguros/`)
+  const insurance = await res.json()
+  const ids = insurance.results.map((item) => item.id)
+  const paths = ids.map((id) => ({ params: { id: id.toString() } }))
+
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export default InsuranceItem
